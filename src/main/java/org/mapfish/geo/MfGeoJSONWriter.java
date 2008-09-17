f3c2de9acc4dcaa38b75bb145546c63d69a8f735
elemoine <elemoine@25cca10d-0837-0410-9e4f-92b931fda1bc>
2008-09-17 12:34:13 +0000

    upgrade to Ext 2.2, this is accompanied by a license change, MapFish Client is now GPL3, while MapFish Server remains LGPL3, the license may change in the future again but the components that use Ext will have to stick with GPL3
    git-svn-id: http://www.mapfish.org/svn/mapfish/trunk/MapFish/server/java/mapfish-geo-lib@1045 25cca10d-0837-0410-9e4f-92b931fda1bc

--- src/main/java/org/mapfish/geo/MfGeoJSONWriter.java
+++ src/main/java/org/mapfish/geo/MfGeoJSONWriter.java
@@ -1,20 +1,20 @@
 /*
  * Copyright (C) 2008  Camptocamp
  *
- * This file is part of MapFish
+ * This file is part of MapFish Server
  *
- * MapFish is free software: you can redistribute it and/or modify
+ * MapFish Server is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * MapFish is distributed in the hope that it will be useful,
+ * MapFish Server is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public License
- * along with MapFish.  If not, see <http://www.gnu.org/licenses/>.
+ * along with MapFish Server.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 /*
